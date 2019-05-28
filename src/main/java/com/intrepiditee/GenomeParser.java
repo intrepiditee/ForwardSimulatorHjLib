d0a1cd6f857eec6f6ca0e6f7df34789bcc87dd31
intrepiditee <jay.shijunjie@gmail.com>
2019-05-28 10:29:00 -0500

    genomeParser phaser implementation

--- src/main/java/com/intrepiditee/GenomeParser.java
+++ src/main/java/com/intrepiditee/GenomeParser.java
@@ -1,7 +1,13 @@
 package com.intrepiditee;
 
+import edu.rice.hj.Module0;
 import edu.rice.hj.Module1;
+import edu.rice.hj.Module2;
+import edu.rice.hj.api.HjPhaser;
+import edu.rice.hj.api.HjPhaserMode;
+import edu.rice.hj.api.HjPhaserPair;
 import edu.rice.hj.api.SuspendableException;
+import edu.rice.hj.runtime.config.HjSystemProperty;
 
 import java.io.EOFException;
 import java.io.File;
@@ -11,7 +17,7 @@ import java.util.*;
 import java.util.concurrent.ConcurrentHashMap;
 import java.util.concurrent.atomic.AtomicInteger;
 
-import static edu.rice.hj.Module0.launchHabaneroApp;
+import static edu.rice.hj.Module0.*;
 
 public class GenomeParser {
 
@@ -24,67 +30,88 @@ public class GenomeParser {
 
             System.out.println();
 
-            // Read in all sequences
-            for (int n = 0; n < numGenerations; n++) {
-                int count = 0;
+            int nThreads = Integer.parseInt(HjSystemProperty.numWorkers.getPropertyValue());
 
-                String filename = "Generation" + n;
-                ObjectInputStream i = Utils.getObjectInputStream(filename);
-
-                BitSet prevGenome = null;
-                try {
-                    while (true) {
-                        int id = i.readInt();
-
-                        BitSet paternalGenome = (BitSet) (i.readObject());
-                        BitSet maternalGenome = (BitSet) (i.readObject());
-                        if (prevGenome != null) {
-                            BitSet prevGenomeFinal = prevGenome;
-                            Module1.forallChunked(0, Individual.genomeLength - 1, (j) -> {
-                                boolean paternalBase = paternalGenome.get(j);
-                                if (prevGenomeFinal.get(j) != paternalBase) {
-                                    isDifferent[j] = true;
-                                } else if (paternalBase != maternalGenome.get(j)) {
-                                    isDifferent[j] = true;
-                                }
-                            });
-                        }
-
-                        prevGenome = maternalGenome;
-
-                        count += 2;
-                        if (count % 1000 == 0) {
-                            System.out.println("Generation" + n + ": " + count / 1000 + "k processed");
-                        }
-
-                    }
-                } catch (EOFException e) {
-                    System.out.println(filename + " parsed");
-                } catch (IOException | ClassNotFoundException e) {
-                    e.printStackTrace();
-                    System.exit(-1);
-                }
-
-                try {
-                    i.close();
-                } catch (IOException e) {
-                    e.printStackTrace();
-                    System.exit(-1);
-                }
-            }
+            final BitSet[] prevGenome = new BitSet[1];
+            final BitSet[] genome = new BitSet[1];
 
             AtomicInteger numDifferentIndices = new AtomicInteger(0);
-            Module1.forallChunked(0, Individual.genomeLength - 1, (i) -> {
-                if (isDifferent[i]) {
-                    numDifferentIndices.incrementAndGet();
+
+            int numBasesPerThread = Individual.genomeLength / nThreads;
+
+            forallPhased(0, nThreads - 1, (i) -> {
+                int start = numBasesPerThread * i;
+                int end = i == nThreads - 1 ? Individual.genomeLength : start + numBasesPerThread;
+
+                for (int n = 0; n < numGenerations; n++) {
+                    String filename = "Generation" + n;
+                    ObjectInputStream in = i == 0 ? in = Utils.getObjectInputStream(filename) : null;
+
+                    AtomicInteger count = new AtomicInteger(0);
+
+                    boolean isInt = true;
+
+                    while (true) {
+
+                        if (i == 0) {
+                            try {
+                                if (prevGenome[0] == null) {
+                                    in.readInt();
+                                    prevGenome[0] = (BitSet) in.readObject();
+                                    count.incrementAndGet();
+                                } else {
+                                    if (isInt) {
+                                        in.readInt();
+                                        isInt = false;
+                                    } else {
+                                        isInt = true;
+                                    }
+                                }
+
+                                genome[0] = (BitSet) in.readObject();
+                            } catch (EOFException e) {
+                                prevGenome[0] = null;
+                            } catch (IOException | ClassNotFoundException e) {
+                                e.printStackTrace();
+                                System.exit(-1);
+                            }
+                        }
+
+                        next();
+
+                        if (prevGenome[0] == null) {
+                            break;
+                        }
+
+                        for (int j = start; j < end; j++) {
+                            if (prevGenome[0].get(j) != genome[0].get(j)) {
+                                isDifferent[j] = true;
+                            }
+                        }
+
+                        next();
+
+                        if (i == 0) {
+                            prevGenome[0] = genome[0];
+                            int c = count.incrementAndGet();
+                            if (c % 1000 == 0) {
+                                System.out.println("Generation" + n + ": " + c / 1000 + "k processed");
+                            }
+                        }
+                    }
+
                 }
+
+                for (int k = start; k < end; k++) {
+                    if (isDifferent[k]) {
+                        numDifferentIndices.incrementAndGet();
+                    }
+                }
+
             });
 
             System.out.println(numDifferentIndices.get());
         });
-
-
-
 
 
     }
