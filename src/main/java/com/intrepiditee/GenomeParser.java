3b8cda755496fdeced7f11fc053598b39bee8ae5
intrepiditee <jay.shijunjie@gmail.com>
2019-05-30 14:19:39 -0500

    VCF ref updated

--- src/main/java/com/intrepiditee/GenomeParser.java
+++ src/main/java/com/intrepiditee/GenomeParser.java
@@ -1,13 +1,14 @@
 package com.intrepiditee;
 
 import edu.rice.hj.api.SuspendableException;
+import edu.rice.hj.runtime.config.HjSystemProperty;
+import javafx.util.Pair;
 
 import java.io.*;
-import java.nio.charset.StandardCharsets;
 import java.util.*;
-import java.util.zip.GZIPOutputStream;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
-import static com.intrepiditee.Configs.numGenerations;
 import static edu.rice.hj.Module0.*;
 
 public class GenomeParser {
@@ -15,208 +16,35 @@ public class GenomeParser {
     static int minID = Integer.MAX_VALUE;
     static int maxID = Integer.MIN_VALUE;
 
-    static Integer[] variantSiteIndices;
+    static Set<Integer> variantSiteIndices = Collections.newSetFromMap(new ConcurrentHashMap<>());
 
-    @SuppressWarnings("unchecked")
+    static Map<Pair<Integer, Integer>, Byte> idIndexPairToEncoding = new ConcurrentHashMap<>();
+
+    static Map<Byte, String> genotypeEncoding = new HashMap<>();
+
     public static void main(String[] args) {
-        if (args.length < 6 || (!args[0].equals("--parse"))) {
-            System.err.println(
-                "Usage: bash run.sh --parse genomeLength generationSize numberOfGenerationsStored" +
-                    " exclusiveLowerBound numThreads"
-            );
-            System.exit(-1);
-        }
+        int numGenerations = 4;
+        Individual.genomeLength = 10000;
 
-        Configs.genomeLength = Integer.parseInt(args[1]);
-        Configs.generationSize = Integer.parseInt(args[2]);
-        numGenerations = Integer.parseInt(args[3]);
-        int lowerBound = Integer.parseInt(args[4]);
-        Configs.numThreads = Integer.parseInt(args[5]);
-
-        File f = Utils.getFile("variantSiteIndices");
-        if (f.exists()) {
-            try {
-                System.out.println("\nvariantSiteIndices file exists");
-                ObjectInputStream in = Utils.getBufferedObjectInputStream("variantSiteIndices");
-                minID = in.readInt();
-                maxID = in.readInt();
-                variantSiteIndices = (Integer[]) in.readUnshared();
-                in.close();
-                System.out.println("variantSiteIndices file read");
-                System.out.println();
-
-            } catch (IOException | ClassNotFoundException e) {
-                e.printStackTrace();
-                System.exit(-1);
-            }
-        }
+        genotypeEncoding.put((byte) 0, "0|0");
+        genotypeEncoding.put((byte) 1, "0|1");
+        genotypeEncoding.put((byte) 2, "1|0");
+        genotypeEncoding.put((byte) 3, "1|1");
 
         launchHabaneroApp(() -> {
-            if (!f.exists()) {
-                System.out.println("\nvariantSiteIndices file does not exist");
-                System.out.println();
-                getVariantSitesMoreThan(lowerBound);
-            }
-
-            writeVCF();
+            System.out.println(getNumVariantSites(numGenerations));
+            writeVCF(numGenerations);
         });
 
 
     }
 
-    private static void writeVCF() throws SuspendableException {
-        Arrays.sort(variantSiteIndices);
+    private static void writeVCF(int numGenerations) throws SuspendableException {
+        int bufferSize = 200000;
 
-        int numSites = variantSiteIndices.length;
-        int numSitesPerThread = numSites / Configs.numThreads;
+        PrintWriter out = new PrintWriter(Utils.getBufferedWriter("out.vcf", bufferSize));
 
-        final int[] id = new int[1];
-        final BitSet[] paternalGenome = new BitSet[1];
-        final BitSet[] maternalGenome = new BitSet[1];
-
-        final StringBuilder[] records = new StringBuilder[numSites];
-
-        final String[][] idIndexToBases = new String[maxID - minID + 1][numSites];
-
-        forallPhased(0, Configs.numThreads - 1, (i) -> {
-            int start = numSitesPerThread * i;
-            int end = i == Configs.numThreads - 1 ? numSites : start + numSitesPerThread;
-
-//            System.out.println(start);
-//            System.out.println(end);
-
-            // Store all base pairs at variant sites
-
-            for (int n = 0; n < numGenerations; n++) {
-                String filename = i == 0 ? "Generation" + n : null;
-                ObjectInputStream in = i == 0 ?
-                    Utils.getBufferedObjectInputStream(filename) :
-                    null;
-
-
-                int numProcessed = 0;
-
-                while (true) {
-                    if (i == 0) {
-                        try {
-                            // Read one individual
-                            id[0] = in.readInt();
-                            minID = Math.min(minID, id[0]);
-                            maxID = Math.max(maxID, id[0]);
-
-                            System.out.println(id[0]);
-
-                            paternalGenome[0] = (BitSet) in.readUnshared();
-                            maternalGenome[0] = (BitSet) in.readUnshared();
-
-                        } catch (EOFException e) {
-                            id[0] = -1;
-                            System.out.println("Generation" + n + " processed");
-                        } catch (IOException | ClassNotFoundException e) {
-                            e.printStackTrace();
-                            System.exit(-1);
-                        }
-                    }
-
-                    next();
-
-                    if (id[0] == -1) {
-                        break;
-                    }
-
-                    for (int j = start; j < end; j++) {
-                        int variantSiteIndex = variantSiteIndices[j];
-
-                        boolean paternalBase = paternalGenome[0].get(variantSiteIndex);
-                        boolean maternalBase = maternalGenome[0].get(variantSiteIndex);
-
-                        int idIndex = id[0] - minID;
-                        if (!paternalBase) {
-                            if (!maternalBase) {
-                                idIndexToBases[idIndex][j] = "0|0";
-                            } else {
-                                idIndexToBases[idIndex][j] = "0|1";
-                            }
-                        } else {
-                            if (!maternalBase) {
-                                idIndexToBases[idIndex][j] = "1|0";
-                            } else {
-                                idIndexToBases[idIndex][j] = "1|1";
-                            }
-                        }
-                    }
-
-                    next();
-
-                    if (i == 0) {
-                        numProcessed += 2;
-                        if (numProcessed % 1000 == 0) {
-                            System.out.println("Generation" + n + ": " + numProcessed / 1000 + "k processed");
-                        }
-                    }
-
-                } // end of while
-
-
-                if (i == 0) {
-                    try {
-                        in.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        System.exit(-1);
-                    }
-                }
-
-            } // end of all generations
-
-
-            // Generate all the rows
-
-            int numRecordsGenerated = 0;
-            for (int j = start; j < end; j++) {
-                int variantSiteIndex = variantSiteIndices[j];
-
-                records[j] = new StringBuilder();
-                records[j].append("22\t");
-                records[j].append(variantSiteIndex + 1);
-                records[j].append("\trs");
-                records[j].append(variantSiteIndex + 1);
-                records[j].append("\tA\tC\t.\tPASS\t.\tGT");
-
-                for (int ID = minID; ID <= maxID; ID++) {
-                    int idIndex = ID - minID;
-                    String bases = idIndexToBases[idIndex][j];
-
-                    records[j].append("\t");
-                    records[j].append(bases);
-                }
-
-                if (i == 0) {
-                    numRecordsGenerated += Configs.numThreads;
-                    if (numRecordsGenerated % 1000 == 0) {
-                        System.out.println("Records: " + numRecordsGenerated / 1000 + " k generated");
-                    }
-                }
-            }
-
-        }); // end of parallel part
-
-
-        // Write all the rows sequentially
-
-        PrintWriter out = null;
-        try {
-            GZIPOutputStream zip = new GZIPOutputStream(
-                new FileOutputStream(Utils.createEmptyFile("out.vcf.zip")));
-            BufferedWriter writer = new BufferedWriter(
-                new OutputStreamWriter(zip, StandardCharsets.UTF_8), Utils.bufferSize);
-
-            out = new PrintWriter(writer);
-
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
+        System.out.println();
 
         out.print("#CHROM\tPOS\tID\tREF\tALT\tQUAL\tFILTER\tINFO\tFORMAT");
         for (int i = minID; i <= maxID; i++) {
@@ -224,57 +52,146 @@ public class GenomeParser {
         }
         out.print("\n");
 
+        Integer[] indices = variantSiteIndices.toArray(new Integer[0]);
+        Arrays.sort(indices);
+
+        for (int n = 0; n < numGenerations; n++) {
+            int count = 0;
+
+            String filename = "Generation" + n;
+            ObjectInputStream in = Utils.getBufferedObjectInputStream(filename);
+
+            while (true) {
+                try {
+                    int id = in.readInt();
+                    BitSet paternalGenome = (BitSet) in.readUnshared();
+                    BitSet maternalGenome = (BitSet) in.readUnshared();
+
+                    for (int i : indices) {
+                        boolean paternalBase = paternalGenome.get(i);
+                        boolean maternalBase = maternalGenome.get(i);
+
+                        Pair<Integer, Integer> idIndexPair = new Pair<>(id, i);
+
+                        if (!paternalBase) {
+                            if (!maternalBase) {
+                                // "0/0"
+                                idIndexPairToEncoding.put(idIndexPair, (byte) 0);
+                            } else {
+                                // "0/1"
+                                idIndexPairToEncoding.put(idIndexPair, (byte) 1);
+                            }
+                        } else {
+                            if (!maternalBase) {
+                                // "1/0"
+                                idIndexPairToEncoding.put(idIndexPair, (byte) 2);
+                            } else {
+                                // "1/1"
+                                idIndexPairToEncoding.put(idIndexPair, (byte) 3);
+                            }
+                        }
+
+                    }
+
+                    count += 2;
+                    if (count % 1000 == 0) {
+                        System.out.println("Generation" + n + ": " + count / 1000 + "k processed");
+                    }
+
+
+                } catch (EOFException e) {
+                    break;
+                } catch (IOException | ClassNotFoundException e) {
+                    e.printStackTrace();
+                    System.exit(-1);
+                }
+            }
+
+            try {
+                in.close();
+            } catch (IOException e) {
+                e.printStackTrace();
+                System.exit(-1);
+            }
+        }
+
+
         int count = 0;
-        for (int i = 0; i < records.length; i++) {
-            out.println(records[i].toString());
-            records[i] = null;
+
+        for (int i : indices) {
+            StringBuilder s = new StringBuilder();
+            s.append("22\t");
+            s.append(i + 1);
+            s.append("\trs");
+            s.append(i + 1);
+            s.append("\tA\tC\t.\tPASS\t.\tGT");
+
+            for (int id = minID; id <= maxID; id++) {
+                byte encoding = idIndexPairToEncoding.get(new Pair<>(id, i));
+
+                s.append("\t");
+                s.append(genotypeEncoding.get(encoding));
+            }
+            out.println(s.toString());
 
             count += 1;
             if (count % 1000 == 0) {
-                System.out.println("Records: " + count / 1000 + " k written");
+                System.out.println("Sites: " + count / 1000 + " k written");
             }
         }
 
         out.close();
+
     }
 
-    private static void getVariantSitesMoreThan(int lowerBound) throws SuspendableException {
+    private static int getNumVariantSites(int numGenerations) throws SuspendableException {
+
+        System.out.println();
+
+        int nThreads = Integer.parseInt(HjSystemProperty.numWorkers.getPropertyValue());
+
+        final BitSet[] prevGenome = new BitSet[1];
         final BitSet[] genome = new BitSet[1];
 
-        int numBasesPerThread = Configs.genomeLength / Configs.numThreads;
+        int numBasesPerThread = Individual.genomeLength / nThreads;
 
-        int[] variantSiteCounts = new int[Configs.genomeLength];
-        List<Integer> variantSiteIndicesArray = new ConcurrentArrayList<>();
-
-        forallPhased(0, Configs.numThreads - 1, (i) -> {
+        forallPhased(0, nThreads - 1, (i) -> {
             int start = numBasesPerThread * i;
-            int end = i == Configs.numThreads - 1 ? Configs.genomeLength : start + numBasesPerThread;
-
-//            System.out.println(start);
-//            System.out.println(end);
+            int end = i == nThreads - 1 ? Individual.genomeLength : start + numBasesPerThread;
 
             for (int n = 0; n < numGenerations; n++) {
                 String filename = "Generation" + n;
-                ObjectInputStream in = i == 0 ? Utils.getBufferedObjectInputStream(filename) : null;
+                ObjectInputStream in = i == 0 ? in = Utils.getBufferedObjectInputStream(filename) : null;
 
-                int count = 0;
+                AtomicInteger count = new AtomicInteger(0);
 
                 boolean isInt = true;
 
                 while (true) {
                     if (i == 0) {
                         try {
-                            if (isInt) {
+                            if (prevGenome[0] == null) {
                                 int id = in.readInt();
                                 minID = Math.min(minID, id);
                                 maxID = Math.max(maxID, id);
+
+                                prevGenome[0] = (BitSet) in.readUnshared();
+                                count.incrementAndGet();
+                            } else {
+                                if (isInt) {
+                                    int id = in.readInt();
+                                    minID = Math.min(minID, id);
+                                    maxID = Math.max(maxID, id);
+
+                                    isInt = false;
+                                } else {
+                                    isInt = true;
+                                }
                             }
-                            isInt = !isInt;
 
                             genome[0] = (BitSet) in.readUnshared();
-
                         } catch (EOFException e) {
-                            genome[0] = null;
+                            prevGenome[0] = null;
                             System.out.println("Generation" + n + " preprocessed");
                         } catch (IOException | ClassNotFoundException e) {
                             e.printStackTrace();
@@ -284,71 +201,31 @@ public class GenomeParser {
 
                     next();
 
-                    if (genome[0] == null) {
+                    if (prevGenome[0] == null) {
                         break;
                     }
 
                     for (int j = start; j < end; j++) {
-                        variantSiteCounts[j] += genome[0].get(j) ? 1 : 0;
-                    }
-
-                    if (i == 0) {
-                        count++;
-                        if (count % 1000 == 0) {
-                            System.out.println("Generation" + n + ": " + count / 1000 + "k preprocessed");
+                        if (prevGenome[0].get(j) != genome[0].get(j)) {
+                            variantSiteIndices.add(j);
                         }
                     }
 
                     next();
-                }
 
-                if (i == 0) {
-                    try {
-                        in.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                        System.exit(-1);
+                    if (i == 0) {
+                        prevGenome[0] = genome[0];
+                        int c = count.incrementAndGet();
+                        if (c % 1000 == 0) {
+                            System.out.println("Generation" + n + ": " + c / 1000 + "k preprocessed");
+                        }
                     }
                 }
 
             }
 
-            int numGenomes = Configs.numGenerations * Configs.generationSize * 2;
-
-            for (int j = start; j < end; j++) {
-
-                int numOnes = variantSiteCounts[j];
-                int minorityCount = numOnes;
-
-                if (numOnes >= numGenomes / 2) {
-                    minorityCount = numGenomes - numOnes;
-                }
-
-                if (minorityCount > lowerBound) {
-                    variantSiteIndicesArray.add(j);
-                }
-            }
-
         });
 
-        variantSiteIndices = variantSiteIndicesArray.toArray(new Integer[0]);
-
-        System.out.println("\nPreprocessing completed");
-        System.out.println("Preprocessing summary:");
-        System.out.println("Number of variant sites: " + variantSiteIndicesArray.size());
-
-        System.out.println("Writing variantSiteIndices to file");
-        ObjectOutputStream o = Utils.getBufferedObjectOutputStream("variantSiteIndices");
-        try {
-            o.writeInt(minID);
-            o.writeInt(maxID);
-            o.writeUnshared(variantSiteIndices);
-            o.close();
-        } catch (IOException e) {
-            e.printStackTrace();
-            System.exit(-1);
-        }
-        System.out.println("variantSiteIndices written to file\n");
+        return variantSiteIndices.size();
     }
-
 }
