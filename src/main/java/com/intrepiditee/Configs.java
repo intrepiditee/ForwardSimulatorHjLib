b522ca9c89da9252ee502b66a9762f2ed1d289c4
intrepiditee <jay.shijunjie@gmail.com>
2019-05-31 16:10:01 -0500

    numThreads

--- src/main/java/com/intrepiditee/Configs.java
+++ src/main/java/com/intrepiditee/Configs.java
@@ -2,7 +2,7 @@ package com.intrepiditee;
 
 public class Configs {
 
-    static int numThreads = 4;
+    static int numThreads = 20;
     static int genomeLength = 51304566;
     static int numGenerations = 50;
     static int numGenerationsStore = 4;
